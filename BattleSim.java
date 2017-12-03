af660627cc5326ca5488cecc8b4d80fba7ccf91f
jadseloberlin <jseligma@oberlin.edu>
2017-12-03 16:38:52 -0500

    finished types
    also, there's an error in our dataset. at least 1. onix is missing at least one value but i can't tell which

--- BattleSim.java
+++ BattleSim.java
@@ -7,24 +7,24 @@ import java.util.Scanner;
 import java.util.Set;
 
 public class BattleSim {
-
+	
 	String[] types = {"fire", "water", "grass", "bug", "ghost","psychic","dragon","electric", "rock", "ice", "poison", "normal", "ground","fighting", "flying","none"};
 	HashMap<String, Mon> validMon = new HashMap<String, Mon>();
-
-	Mon[] team1 = new Mon[3];
-	Mon[] team2 = new Mon[3];
-
+	
+	Mon[] team1 = new Mon[3]; 
+	Mon[] team2 = new Mon[3]; 
+	
 	HashMap<String, HashMap<String, Double>> atkMult = new HashMap<String, HashMap<String, Double>>(); //input attacking type to get a hashmap whose key is the defending type, to get the multiplier
 	//anything attacking type none is x1
-
+	
 	AI p1; //player input, simple original, nn
 	AI p2;
-
+	
 	//Mon activeP1;
 	//Mon activeP2;
-
+	
 	//labels: quick attack, strong attack, switch to 1st available, switch to 2nd available
-
+	
 	public HashMap<String,Double> createBasicMap(){
 		HashMap<String, Double> rtn = new HashMap<String, Double>();
 		for(String type : this.types) {
@@ -35,58 +35,8 @@ public class BattleSim {
 	//varXY represents player X's Y'th pokemon
 	public BattleSim(String p1AI, String p2AI, String p1Mon1, String p1Mon2, String p1Mon3, String p2Mon1, String p2Mon2, String p2Mon3) {
 		//populate type matchups
-		HashMap<String, Double> normalAttack = createBasicMap();
-		normalAttack.put("rock", .5);
-		normalAttack.put("steel", .5);
-		normalAttack.put("ghost", 0.0);
-		this.atkMult.put("normal", normalAttack);
-
-		HashMap<String, Double> fightingAttack = createBasicMap();
-		fightingAttack.put("normal", 2.0);
-		fightingAttack.put("ice", 2.0);
-		fightingAttack.put("dark",2.0);
-		fightingAttack.put("rock", 2.0);
-		fightingAttack.put("steel", 2.0);
-		fightingAttack.put("poison", .5);
-		fightingAttack.put("flying", .5);
-		fightingAttack.put("bug", .5);
-		fightingAttack.put("fairy", .5);
-		fightingAttack.put("psychic", .5);
-		fightingAttack.put("ghost", 0.0);
-		this.atkMult.put("fighting", fightingAttack);
-
-		HashMap<String, Double> flyingAttack = createBasicMap();
-		flyingAttack.put("bug", 2.0);
-		flyingAttack.put("fighting", 2.0);
-		flyingAttack.put("grass", 2.0);
-		flyingAttack.put("electric", .5);;
-		flyingAttack.put("rock", .5);
-		flyingAttack.put("steel", .5);
-		fightingAttack.put("ghost",0.0);
-		this.atkMult.put("flying", flyingAttack);
-
-		HashMap<String, Double> poisonAttack = createBasicMap();
-		poisonAttack.put("fairy", 2.0);
-		poisonAttack.put("grass", 2.0);
-		poisonAttack.put("poison", .5);
-		poisonAttack.put("ground", .5);;
-		poisonAttack.put("rock", .5);
-		poisonAttack.put("ghost", .5);
-		poisonAttack.put("steel", 0.0);
-		this.atkMult.put("poison", poisonAttack);
-
-		HashMap<String, Double> groundAttack = createBasicMap();
-		groundAttack.put("electric", 2.0);
-		groundAttack.put("steel", 2.0);
-		groundAttack.put("fire",2.0);
-		groundAttack.put("poison", 2.0);
-		groundAttack.put("rock", 2.0);
-		groundAttack.put("bug", .5);
-		groundAttack.put("grass", .5);
-		groundAttack.put("flying", 0.0);
-		this.atkMult.put("ground", groundAttack);
-
 		HashMap<String, Double> fireAttack = createBasicMap();
+		
 		fireAttack.put("fire", .5);
 		fireAttack.put("water", .5);
 		fireAttack.put("grass", 2.0);
@@ -96,7 +46,7 @@ public class BattleSim {
 		fireAttack.put("steel", 2.0);
 		fireAttack.put("dragon", .5);
 		this.atkMult.put("fire", fireAttack);
-
+		
 		HashMap<String, Double> waterAttack = createBasicMap();
 		waterAttack.put("dragon", .5);
 		waterAttack.put("grass", .5);
@@ -105,7 +55,7 @@ public class BattleSim {
 		waterAttack.put("ground", 2.0);
 		waterAttack.put("water", .5);
 		this.atkMult.put("water", waterAttack);
-
+		
 		HashMap<String, Double> grassAttack = createBasicMap();
 		grassAttack.put("water", 2.0);
 		grassAttack.put("rock", 2.0);
@@ -118,7 +68,7 @@ public class BattleSim {
 		grassAttack.put("flying", .5);
 		grassAttack.put("steel", .5);
 		this.atkMult.put("grass", grassAttack);
-
+		
 		HashMap<String, Double> electricAttack = createBasicMap();
 		electricAttack.put("water", 2.0);
 		electricAttack.put("ground", 0.0);
@@ -127,16 +77,141 @@ public class BattleSim {
 		electricAttack.put("electric", .5);
 		electricAttack.put("grass", .5);
 		this.atkMult.put("electric", electricAttack);
-
+		
+		HashMap<String, Double> iceAttack = createBasicMap();
+		iceAttack.put("ice", .5);
+		iceAttack.put("steel", .5);
+		iceAttack.put("water", .5);
+		iceAttack.put("fire", .5);
+		iceAttack.put("dragon", 2.0);
+		iceAttack.put("flying", 2.0);
+		iceAttack.put("grass", 2.0);
+		iceAttack.put("ground", 2.0);
+		this.atkMult.put("ice", iceAttack);
+		
 		HashMap<String, Double> ghostAttack = createBasicMap();
 		ghostAttack.put("ghost", 2.0);
 		ghostAttack.put("psychic", 2.0);
 		ghostAttack.put("dark", .5);
-		ghostAttack.put("steel", .5);;
 		ghostAttack.put("normal", 0.0);
 		this.atkMult.put("ghost", ghostAttack);
-
-
+		
+		HashMap<String, Double> groundAttack = createBasicMap();
+		groundAttack.put("electric", 2.0);
+		groundAttack.put("fire", 2.0);
+		groundAttack.put("poison", 2.0);
+		groundAttack.put("rock", 2.0);
+		groundAttack.put("steel", 2.0);
+		groundAttack.put("bug", .5);
+		groundAttack.put("grass", .5);
+		groundAttack.put("flying", 0.0);
+		this.atkMult.put("ground", groundAttack);
+		
+		HashMap<String, Double> fairyAttack  = createBasicMap();
+		fairyAttack.put("dark", 2.0);
+		fairyAttack.put("dragon", 2.0);
+		fairyAttack.put("fighting", 2.0);
+		fairyAttack.put("fire", .5);
+		fairyAttack.put("poison", .5);
+		fairyAttack.put("steel", .5);
+		this.atkMult.put("fairy", fairyAttack);
+		
+		HashMap<String, Double> bugAttack = createBasicMap();
+		bugAttack.put("dark", 2.0);
+		bugAttack.put("grass", 2.0);
+		bugAttack.put("psychic", 2.0);
+		bugAttack.put("fairy", .5);
+		bugAttack.put("fighting", .5);
+		bugAttack.put("flying", .5);
+		bugAttack.put("ghost", .5);
+		bugAttack.put("poison", .5);
+		bugAttack.put("fire", .5);
+		bugAttack.put("steel", .5);
+		this.atkMult.put("bug", bugAttack);
+		
+		HashMap<String, Double> psychicAttack = createBasicMap();
+		psychicAttack.put("fighting", 2.0);
+		psychicAttack.put("poison", 2.0);
+		psychicAttack.put("psychic", .5);
+		psychicAttack.put("steel", .5);
+		psychicAttack.put("dark", 0.0);
+		this.atkMult.put("psychic", psychicAttack);
+		
+		HashMap<String, Double> poisonAttack = createBasicMap();
+		poisonAttack.put("fairy", 2.0);
+		poisonAttack.put("grass", 2.0);
+		poisonAttack.put("ground", .5);
+		poisonAttack.put("rock", .5);
+		poisonAttack.put("ghost", .5);
+		poisonAttack.put("poison", .5);
+		poisonAttack.put("steel", 0.0);
+		this.atkMult.put("poison", poisonAttack);
+		
+		HashMap<String, Double> rockAttack = createBasicMap();
+		rockAttack.put("bug", 2.0);
+		rockAttack.put("fire", 2.0);
+		rockAttack.put("flying", 2.0);
+		rockAttack.put("ice", 2.0);
+		rockAttack.put("fighting", .5);
+		rockAttack.put("ground",.5);
+		rockAttack.put("steel", .5);
+		this.atkMult.put("rock", rockAttack);
+		
+		HashMap<String, Double> flyingAttack = createBasicMap();
+		flyingAttack.put("bug", 2.0);
+		flyingAttack.put("fighting", 2.0);
+		flyingAttack.put("grass", 2.0);
+		flyingAttack.put("electric", .5);
+		flyingAttack.put("rock", .5);
+		flyingAttack.put("steel", .5);
+		this.atkMult.put("flying", flyingAttack);
+		
+		HashMap<String, Double> normalAttack = createBasicMap();
+		normalAttack.put("rock", .5);
+		normalAttack.put("steel", .5);
+		normalAttack.put("ghost", 0.0);
+		this.atkMult.put("normal", normalAttack);
+		
+		HashMap<String, Double> steelAttack = createBasicMap();
+		steelAttack.put("fairy",2.0);
+		steelAttack.put("ice",2.0);
+		steelAttack.put("rock", 2.0);
+		steelAttack.put("electric",.5);
+		steelAttack.put("fire", .5);
+		steelAttack.put("steel",.5);
+		steelAttack.put("water", .5);
+		this.atkMult.put("steel", steelAttack);
+		
+		HashMap<String, Double> dragonAttack = createBasicMap();
+		dragonAttack.put("dragon", 2.0);
+		dragonAttack.put("steel", .5);
+		dragonAttack.put("fairy", 0.0);
+		this.atkMult.put("dragon", dragonAttack);
+		
+		HashMap<String, Double> fightingAttack = createBasicMap();
+		fightingAttack.put("normal", 2.0);
+		fightingAttack.put("dark", 2.0);
+		fightingAttack.put("ice",2.0);
+		fightingAttack.put("steel", 2.0);
+		fightingAttack.put("rock",2.0);
+		fightingAttack.put("bug", .5);
+		fightingAttack.put("fairy",.5);
+		fightingAttack.put("flying",.5);
+		fightingAttack.put("poison", .5);
+		fightingAttack.put("psychic", .5);
+		fightingAttack.put("ghost",0.0);
+		this.atkMult.put("fighting", fightingAttack);
+		
+		HashMap<String, Double> darkAttack = createBasicMap();
+		darkAttack.put("ghost", 2.0);
+		darkAttack.put("psychic", 2.0);
+		darkAttack.put("dark", .5);
+		darkAttack.put("fairy", .5);
+		darkAttack.put("fighting", .5);
+		this.atkMult.put("dark", darkAttack);
+		
+		
+		
 		//load AI
 		if(p1AI.equals("user")) {
 			p1 = new UserControl();
@@ -151,7 +226,7 @@ public class BattleSim {
 			System.out.println("invalid ai for player 1");
 			System.exit(1);
 		}
-
+		
 		if(p2AI.equals("user")) {
 			p2 = new BasicAI();
 		}
@@ -165,7 +240,7 @@ public class BattleSim {
 			System.out.println("invalid ai for player 2");
 			System.exit(1);
 		}
-
+		
 		//load validMon
 		Scanner s = null;
 		try {
@@ -177,7 +252,7 @@ public class BattleSim {
 		}
 		s.nextLine();
 		while(s.hasNextLine()) {
-			String[] lineparts = s.nextLine().split(",");
+			String[] lineparts = s.nextLine().split(","); 
 			String name = lineparts[0];
 			validMon.put(name, new Mon(name, Integer.parseInt(lineparts[1]), Integer.parseInt(lineparts[2]),Integer.parseInt(lineparts[3]),
 					Integer.parseInt(lineparts[4]), lineparts[5], Integer.parseInt(lineparts[6]), Integer.parseInt(lineparts[7]),
@@ -201,10 +276,10 @@ public class BattleSim {
 		team2[2]=m23;
 	}
 	public boolean isDefeated(Mon[] team) {//returns whether or not team has no usable Pokemon left
-
+		
 		return team[0].defeated&&team[1].defeated&team[2].defeated;
 	}
-
+	
 	public Mon active(Mon[] team) {
 		if(team[0].active) {
 			return team[0];
@@ -216,7 +291,7 @@ public class BattleSim {
 			return team[2];
 		}
 	}
-
+	
 	public void switch1(Mon[] team) {
 		if(team[0].active) {
 			if(!team[1].defeated) {
@@ -233,7 +308,7 @@ public class BattleSim {
 				return;
 			}
 		}
-
+		
 		else if(team[1].active) {
 			if(!team[2].defeated) {
 				team[1].active=false;
@@ -268,7 +343,7 @@ public class BattleSim {
 			return;
 		}
 	}
-
+	
 	public void switch2(Mon[] team) {
 		if(team[0].active) {
 			if(!team[2].defeated) {
@@ -318,9 +393,9 @@ public class BattleSim {
 			return;
 		}
 	}
-
+	
 	public boolean quick(Mon attacker, Mon target, Mon[] targetTeam) { //returns true if target faints
-
+		
 		int accCheck = new Random().nextInt(100); //accuracy needs to be higher than accCheck to land
 		if(attacker.quickMoveAcc <= accCheck) {
 			return false;
@@ -331,7 +406,7 @@ public class BattleSim {
 		multiplier = multiplier * this.atkMult.get(attacker.quickMoveType).get(target.type2);
 		damage = damage * multiplier;
 		target.hp = target.hp - (int) damage;
-
+		
 		if(target.hp < 1) {
 			target.defeated=true;
 			if(!isDefeated(targetTeam)) {
@@ -340,11 +415,11 @@ public class BattleSim {
 			return true;
 		}
 		return false;
-
+		
 	}
-
+	
 	public boolean strong(Mon attacker, Mon target, Mon[] targetTeam) { //returns true if target faints
-
+		
 		int accCheck = new Random().nextInt(100); //accuracy needs to be higher than accCheck to land
 		if(attacker.strongMoveAcc <= accCheck) {
 			return false;
@@ -359,25 +434,25 @@ public class BattleSim {
 			return true;
 		}
 		return false;
-
+		
 	}
-
+	
 	public boolean battle() {
 		return this.battle(team1, team2);
 	}
-
+	
 	//returns true if p1 wins, false if p2 wins
 	public boolean battle(Mon[] teamNum1, Mon[] teamNum2) {
-
+		
 		while(!isDefeated(teamNum1)&&!isDefeated(teamNum2)) {
-
-
+			
+		
 			Mon active1 = active(teamNum1);
 			Mon active2 = active(teamNum2);
 			String move1 = p1.chooseMove(teamNum1, active2, atkMult);
 			String move2 = p2.chooseMove(teamNum2, active1, atkMult);
 			if(move1.equals("switch1")){
-				//if player 1 is switching to first available
+				//if player 1 is switching to first available 
 				if(move2.equals("switch1")) {
 					//if player 2 is also switching to first
 					if(active1.speed>=active2.speed) {
@@ -389,7 +464,7 @@ public class BattleSim {
 						//if p2 is faster
 						switch1(teamNum2);
 						switch1(teamNum1);
-
+						
 					}
 				}
 				else if(move2.equals("switch2")) {
@@ -403,7 +478,7 @@ public class BattleSim {
 						//if p2 is faster
 						switch2(teamNum2);
 						switch1(teamNum1);
-
+						
 					}
 				}
 				else if(move2.equals("quick")) {
@@ -430,7 +505,7 @@ public class BattleSim {
 						//if p2 is faster
 						switch2(teamNum2);
 						switch2(teamNum1);
-
+						
 					}
 				}
 				else if(move2.equals("switch1")) {
@@ -444,7 +519,7 @@ public class BattleSim {
 						//if p2 is faster
 						switch1(teamNum2);
 						switch2(teamNum1);
-
+						
 					}
 				}
 				else if(move2.equals("quick")) {
@@ -458,7 +533,7 @@ public class BattleSim {
 					strong(active2, active(teamNum1), teamNum1);
 				}
 			}
-
+			
 			else if(move1.equals("quick")) {
 				//p1 chooses quick
 				if(move2.equals("switch1")) {
@@ -500,7 +575,7 @@ public class BattleSim {
 						quick(active(teamNum1), active2, teamNum2);
 					}
 				}
-
+					
 			}
 			else {
 				//p1 chooses strong
@@ -543,58 +618,58 @@ public class BattleSim {
 						strong(active1, active2, teamNum2);
 					}
 				}
-
+				
 			}
-
-
-
-
-
-
-
+			
+					
+			
+			
+			
+			
+			
 		}
-
-
+		
+		
 		return isDefeated(teamNum2);
-
+		
 	}
-
-
+	
+	
 	public static void main(String[] args) {
-
+		
 		BattleSim sim = new BattleSim("basic", "basic", "charizard", "blastoise", "venusaur", "gyarados", "gengar", "arcanine");
 		System.out.println(sim.battle());
-
+		
 	}
-
-
+	
+	
 	/*HashMap<String, Move> validMoves = new HashMap<String, Move>(); //all valid moves in file
-
-
-
+	
+	
+	
 	public BattleSim(ArrayList<Mon> t1, String a1, ArrayList<Mon> t2, String a2) { //FIX THIS
 		team1 = t1;
 		activeP1 = team1.get(0);
 		team2 = t2;
 		activeP2 = team2.get(0);
-
+		
 		p1=pickAI(a1);
 		p2=pickAI(a2);
-
+		
 	}
-
+	
 	public boolean winner() { //true for player 1, false for player 2
 		return(team2.isEmpty());
 	}
-
-
+	
+	
 	public AI pickAI(String input) {
-
-
+		
+		
 	}
-
+	
 	public String pickMove(int playerNum) { //1 for p1, 2 for p2
-
+		
 		AI player = p1;
 		ArrayList<Mon> team = team1;
 		Mon active = activeP1;
@@ -609,9 +684,9 @@ public class BattleSim {
 		}
 		String moveName = player.chooseMove(team, active, oppTeam, activeOpp, this.validMoves, atkMult);
 		return moveName;
-
+		
 	}
-
+	
 	private void switchMon(int playerNum) {
 		AI player = p1;
 		ArrayList<Mon> team = team1;
@@ -632,37 +707,37 @@ public class BattleSim {
 		else {
 			this.activeP2=newActive;
 		}
-
+		
 	}
-
+	
 	public void fainted(int playerNum, Mon knockedOut) {
 		ArrayList<Mon> team = team1;
 		if(playerNum==2) {
 			team = team2;
-		}
+		}		
 		switchMon(playerNum);
 		team.remove(0);
 	}
-
+	
 	public boolean battleComplete() {
 		return(team1.isEmpty()||team2.isEmpty());
 	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		
 		//process mon, move data
-
+		
 		//process args
 
 		BattleSim sim = new BattleSim();
 		//run battle
 		while(!sim.battleComplete()) {
-
+			
 			//handle edge cases appropriately
-
+			
 			//KEEP A LOG OF BATTLE
-
+			
 			//calculate who moves first
 			String p1Move = sim.pickMove(1);
 			String p2Move = sim.pickMove(2);
@@ -679,7 +754,7 @@ public class BattleSim {
 						sim.switchMon(2);
 						sim.switchMon(1);
 					}
-
+					
 				}
 				else {
 					//only p1 switches
@@ -689,7 +764,7 @@ public class BattleSim {
 						//p2 ko's p1
 						sim.fainted(1, sim.activeP1);
 					}
-
+					
 				}
 			}
 			else if( p2Move.equals("SWITCH")) {
@@ -702,7 +777,7 @@ public class BattleSim {
 				}
 			}
 			else {
-
+				
 				//no one switches
 				Move move1 = sim.validMoves.get(p1Move);
 				Move move2 = sim.validMoves.get(p2Move);
@@ -718,7 +793,7 @@ public class BattleSim {
 							//p2 ko's p1
 							sim.fainted(1, sim.activeP1);
 						}
-
+						
 					}
 				}
 				else if(move2.priority()>move1.priority()) {
@@ -749,7 +824,7 @@ public class BattleSim {
 								//p2 ko's p1
 								sim.fainted(1, sim.activeP1);
 							}
-
+							
 						}
 					}
 					else {
@@ -767,16 +842,16 @@ public class BattleSim {
 						}
 					}
 				}
-
+				
 			}
-
+			
 		}
-
+		
 		boolean winner = sim.winner();
-
-
+		
+		
 	}*/
 
-
+	
 
 }
