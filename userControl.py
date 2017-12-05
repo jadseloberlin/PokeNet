5c9112fb35d89239597944ed30340c0a68318258
hdiggins <hdiggins@oberlin.edu>
2017-12-05 17:38:21 -0500

    fixed some things

--- userControl.py
+++ userControl.py
@@ -3,9 +3,9 @@ import ai as AI
 class UserControl(object):
     def __init__(self):
 
-	def chooseMove(ArrayList<Mon> team, Mon active, ArrayList<Mon> oppTeam, Mon activeOpp,
-			HashMap<String, Move> validMoves, HashMap<String, HashMap<String, Double>> typeMatchups):
-
-
-	def chooseSwitch(ArrayList<Mon> team, Mon active, ArrayList<Mon> oppTeam, Mon activeOpp,
-			HashMap<String, Move> validMoves, HashMap<String, HashMap<String, Double>> typeMatchups):
+	# def chooseMove(ArrayList<Mon> team, Mon active, ArrayList<Mon> oppTeam, Mon activeOpp,
+	# 		HashMap<String, Move> validMoves, HashMap<String, HashMap<String, Double>> typeMatchups):
+    #
+    #
+	# def chooseSwitch(ArrayList<Mon> team, Mon active, ArrayList<Mon> oppTeam, Mon activeOpp,
+	# 		HashMap<String, Move> validMoves, HashMap<String, HashMap<String, Double>> typeMatchups):
