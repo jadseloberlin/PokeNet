de5a64569c4b38a76117ba568a75fc26f8019174
hdiggins <hdiggins@oberlin.edu>
2017-12-06 15:32:53 -0500

    added usercontrol features

--- userControl.py
+++ userControl.py
@@ -1,7 +1,11 @@
-import ai as AI
+import sys
 
 class UserControl(object):
     def __init__(self):
+
+    def chooseMove(self, team, activeOpp, typeMatchups):
+        move = input("Please type in move from move list: switch1, switch2, strong, quick :   "  )
+        return move
 
 	# def chooseMove(ArrayList<Mon> team, Mon active, ArrayList<Mon> oppTeam, Mon activeOpp,
 	# 		HashMap<String, Move> validMoves, HashMap<String, HashMap<String, Double>> typeMatchups):
